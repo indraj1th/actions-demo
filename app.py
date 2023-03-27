8dbd0f27e027fea843c701870e74b2ca51e3e73d
Indrajith Sajeevan <indrajith@qburst.com>
2023-03-27 15:28:09 +0530

    this is basic project

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    priority = db.Column(db.Integer, nullable=True)
+    is_completed = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    tasks = Task.query.all()
+    return render_template('tasks.html', tasks=tasks)
+
+
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+
+        priority = request.form['priority']
+        task = Task(title=title, description=description, priority=priority)
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('add_task.html')
+
+
+@app.route('/complete_task/<int:task_id>')
+def complete_task(task_id):
+    task = Task.query.get(task_id)
+    task.is_completed = True
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete_task/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
